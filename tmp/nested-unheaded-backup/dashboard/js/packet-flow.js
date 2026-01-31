bb9c38b05f685f2fc77b1518ed977129dc1f48fb
bellistech <stevie@bellis.tech>
2026-01-31 12:04:32 -0600

    cleanup

--- tmp/nested-unheaded-backup/dashboard/js/packet-flow.js
+++
@@ -1,4 +0,0 @@
-// Real-time packet flow visualization
-// TODO: WebSocket connection to dashboard-backend
-// TODO: D3.js network graph
-console.log('packet-flow.js loaded');
--- tmp/nested-unheaded-backup/nix/tests/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/unheaded/unheaded/nix/tests
-
-go 1.21
-
-// No external dependencies - using only stdlib for tests
